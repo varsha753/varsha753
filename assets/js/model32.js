e825e43a240c56f6f7aa717e518f7f383019b26b
varsha753 <varsha.rani@yahoo.com>
2021-06-27 22:51:53 +0530

    new-3

--- assets/js/model32.js
+++ assets/js/model32.js
@@ -250,7 +250,7 @@ document.getElementById("time").textContent = "00:00";
 
     }
     else{
-      poseLabel='Hold your pose';
+      //poseLabel='Hold your pose';
       counter+=1;
       document.getElementById("hold").textContent = "Hold this pose";
       if (counter<9)
@@ -349,73 +349,6 @@ else if(navigator.getUserMedia) { // Standard
 //}, 10000);
 
 
-function secondPlay() {
-$("body").removeClass("play");
-var aa = $("ul.secondPlay li.active");
-
-if (aa.html() == undefined) {
-    aa = $("ul.secondPlay li").eq(0);
-    aa.addClass("before")
-        .removeClass("active")
-        .next("li")
-        .addClass("active")
-        .closest("body")
-        .addClass("play");
-
-}
-else if (aa.is(":last-child")) {
-    $("ul.secondPlay li").removeClass("before");
-    aa.addClass("before").removeClass("active");
-    aa = $("ul.secondPlay li").eq(0);
-    aa.addClass("active")
-        .closest("body")
-        .addClass("play");
-}
-else {
-    $("ul.secondPlay li").removeClass("before");
-    aa.addClass("before")
-        .removeClass("active")
-        .next("li")
-        .addClass("active")
-        .closest("body")
-        .addClass("play");
-}
-
-}
-
-function minutePlay() {
-$("body").removeClass("play");
-var aa = $("ul.minutePlay li.active");
-
-if (aa.html() == undefined) {
-    aa = $("ul.minutePlay li").eq(0);
-    aa.addClass("before")
-        .removeClass("active")
-        .next("li")
-        .addClass("active")
-        .closest("body")
-        .addClass("play");
-
-}
-else if (aa.is(":last-child")) {
-    $("ul.minutePlay li").removeClass("before");
-    aa.addClass("before").removeClass("active");
-    aa = $("ul.minutePlay li").eq(0);
-    aa.addClass("active")
-        .closest("body")
-        .addClass("play");
-}
-else {
-    $("ul.minutePlay li").removeClass("before");
-    aa.addClass("before")
-        .removeClass("active")
-        .next("li")
-        .addClass("active")
-        .closest("body")
-        .addClass("play");
-}
-
-}
 
 
 function draw() {
